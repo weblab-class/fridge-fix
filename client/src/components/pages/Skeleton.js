eacb1e13e6822474026b5a4354b5f0932fd71825
Zawad <zawadx@gmail.com>
2020-01-17 21:34:18 -0500

    set up google sign in?

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -4,8 +4,7 @@ import GoogleLogin, { GoogleLogout } from "react-google-login";
 import "../../utilities.css";
 import "./Skeleton.css";
 
-//TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "848241716739-mbsjshm9umshpbg7hu2cnntrkcdd1gf3.apps.googleusercontent.com";
 
 class Skeleton extends Component {
   constructor(props) {
@@ -46,12 +45,9 @@ class Skeleton extends Component {
         <h2> What you need to change</h2>
         <ul>
           <li>Change the font in utilities.css</li>
-          <li>Change the Frontend CLIENT_ID for Google Auth (Skeleton.js)</li>
-          <li>Change the Server CLIENT_ID for Google Auth (auth.js)</li>
           <li>Change the Database SRV for Atlas (server.js)</li>
           <li>Change the Database Name for MongoDB (server.js)</li>
           <li>Add a favicon to your website at the path client/dist/favicon.ico</li>
-          <li>Update website title in client/dist/index.html</li>
         </ul>
       </>
     );
