76643c3b15fbd67a7fdb1f6b3d307f07189e05f8
smalltext <leepong.amerman@gmail.com>
2020-01-21 18:19:32 -0500

    Revert "Mary lau/shop"

--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -12,7 +12,7 @@ class Feed extends Component {
   componentDidMount() {
 
   }
-  
+
   render() {
     return (
       <div>
---
+++ client/src/components/pages/FridgeList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class FridgeList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>FridgeList</h1>
+        <p>The page you requested couldn't be found.</p>
+      </div>
+    );
+  }
+}
+
+export default FridgeList;
