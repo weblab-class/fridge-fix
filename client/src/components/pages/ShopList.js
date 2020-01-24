0e770abd1220873075ec9f446f69af571df2ad78
Zawad <zawadx@gmail.com>
2020-01-23 21:01:35 -0500

    progress

--- client/src/components/pages/ShopList.js
+++ client/src/components/pages/ShopList.js
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
 import IngredientSearch from "../modules/IngredientSearch.js";
+import "./ShopList.css";
+import ShopListItem from "../modules/ShopListItem.js";
+
+const testShopList = [
+  {
+    ingredientID: "onion",
+    qt: 1,
+  },
+  {
+    ingredientID: "egg",
+    qt: 12,
+  },
+  {
+    ingredientID: "bacon",
+    qt: 2,
+  },
+]
+
+
 
 class ShopList extends Component {
   constructor(props) {
     super(props);
+
+    this.state={
+      items: testShopList
+    }
   }
 
   render() {
+    let itemList = null;
+    itemList = this.state.items.map((itemObj) => (
+      <ShopListItem
+        ingredientID = {itemObj.ingredientID}
+        qt = {itemObj.qt}
+      />
+    ));
+
     return (
-      <div>
-        <h1>Shopping List</h1>
+      <div className="ShopList-container">
+        <div className="ShopList-bgbox">
+          <h1 className="ShopList-title">Shopping List</h1>
+          {itemList}
+        </div>
         <IngredientSearch targetList="shop" />
       </div>
     );
