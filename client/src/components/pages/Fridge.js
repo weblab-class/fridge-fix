32c96f8f239a557033da8355ab1f27659ddb2a3d
Mary Lau <marylau@mit.edu>
2020-01-23 22:22:50 -0500

    added Fridge related files

---
+++ client/src/components/pages/Fridge.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import FridgeList from "../modules/FridgeList.js";
+import RecipeFeed from "../modules/RecipeFeed.js";
+
+import "./feed.css";
+
+class Fridge extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  componentDidMount() {
+
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className= "feed-container">
+          <span className="feed-item"><FridgeList /></span>
+          <span className="feed-item"><RecipeFeed /></span>
+        </div>
+      </div>
+    );
+  }
+}
+  
+export default Feed;
